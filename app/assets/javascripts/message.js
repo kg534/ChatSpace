3318d4eefc23ee3605d285eed84ebbd94d5c8010
kg534 <kg-katsu08@i.softbank.jp>
2019-12-21 21:00:34 +0900

    add auto update function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,34 +1,35 @@
 $(function(){
+
   function buildHTML(message){
     if　(message.image.url != null ) {
-      var html = `<div class="chat--main__message--list__content">
-                    <div class="chat--main__message--list__content__top">
-                      <div class="user--name">
-                      ${message.user_name}
-                      </div>
-                      <div class="time--stamp">
-                      ${message.created_at}
-                      </div>
-                    </div>
-                    <p class="chat--main__message--list__content__lower">
-                    ${message.body}
-                    </p>
-                    <img class="lower-message__image" src=${message.image.url} >
-                  </div>`
+      var html = `<div class="chat--main__message--list__content" data-message-id=`  + message.id + `>` +
+                    `<div class="chat--main__message--list__content__top">` +
+                      `<div class="user--name">` +
+                        message.user_name +
+                      `</div>` +
+                      `<div class="time--stamp">` +
+                        message.created_at +
+                      `</div>` +
+                    `</div>` +
+                    `<p class="chat--main__message--list__content__lower">` +
+                      message.body +
+                    `</p>` +
+                    `<img class="lower-message__image" src=${message.image.url} >` +
+                  `</div>`
     } else {
-      var html = `<div class="chat--main__message--list__content">
-                    <div class="chat--main__message--list__content__top">
-                      <div class="user--name">
-                        ${message.user_name}
-                      </div>
-                      <div class="time--stamp">
-                        ${message.created_at}
-                      </div>
-                    </div>
-                  <p class="chat--main__message--list__content__lower">
-                    ${message.body}
-                  </p>
-                </div>`
+      var html = `<div class="chat--main__message--list__content" data-message-id=`  + message.id + `>` +
+                    `<div class="chat--main__message--list__content__top">` +
+                      `<div class="user--name">` +
+                        message.user_name +
+                      `</div>` +
+                      `<div class="time--stamp">` +
+                        message.created_at +
+                      `</div>` +
+                    `</div>` +
+                  `<p class="chat--main__message--list__content__lower">` +
+                    message.body +
+                  `</p>` +
+                `</div>`
     }
     return html;
   }
@@ -55,4 +56,32 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.chat--main__message--list__content:last').data("message-id");
+    $.ajax({
+      url: 'api/messages',
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message);
+        });
+        $('.chat--main__message--list').append(insertHTML);
+        $('.chat--main__message--list').animate({ scrollTop: $('.chat--main__message--list')[0].scrollHeight});
+        $('#new_message')[0].reset();
+        $('.send__btn').prop('disabled', false);
+      }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -12,7 +12,6 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-       binding.pry
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
